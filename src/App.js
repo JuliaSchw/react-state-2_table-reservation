3eae9830af65c6a39fb726c35ca8dbbbbcbfcadc
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-12-05 16:39:56 +0100

    solve challenge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+// ./src/App.js
+import React, { useState } from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  const handleIncrement = () => {
+    setPeople(people + 1);
+  };
+
+  const handleDecrement = () => {
+    setPeople(Math.max(0, people - 1));
+  };
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+        people={people}
+      />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
